734892990706b7e687e20d35f1b20d524c2d9fc9
Bramanda Suwandi <bramanda48@gmail.com>
2022-11-24 07:04:25 +0000

    fix: export decorators

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { CBEvent } from './circuit-breaker.constants';
 export { CircuitBreakerModule } from './circuit-breaker.module';
 export { CircuitBreakerService } from './circuit-breaker.service';
-
+export { CircuitBreakerEvent, CircuitBreakerProtected } from './decorators/circuit-breaker.decorator';
