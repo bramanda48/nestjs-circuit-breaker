133c11bbecfb736c82cf4b2c8222a0a2871fc483
Bramanda Suwandi <bramanda48@gmail.com>
2022-11-24 08:10:37 +0000

    fix: reflector and peer dependencies

--- src/circuit-breaker.accessor.ts
+++ src/circuit-breaker.accessor.ts
@@ -1,11 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { CIRCUIT_BREAKER_EVENT, CIRCUIT_BREAKER_PROTECTED } from './circuit-breaker.constants';
 import { CircuitBreakerEventMetadata, CircuitBreakerMetadata } from './interface/circuit-breaker-metadata.interface';
 
 @Injectable()
 export class CircuitBreakerMetadataAccessor {
-    constructor(private readonly reflector: Reflector) {}
+    constructor(@Inject(Reflector.name) private readonly reflector: Reflector) { }
 
     getCircuitBreakerProtected(
         target: Function,
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { CBEvent } from './circuit-breaker.constants';
 export { CircuitBreakerModule } from './circuit-breaker.module';
 export { CircuitBreakerService } from './circuit-breaker.service';
-export { CircuitBreakerEvent, CircuitBreakerProtected } from './decorators/circuit-breaker.decorator';
+export * from './decorators/circuit-breaker.decorator';
+
