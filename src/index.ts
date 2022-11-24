d28ee65fa9358930df4b6f0ccc3aafbcd6132f71
Bramanda Suwandi <bramanda48@gmail.com>
2022-11-24 08:21:47 +0000

    add: export CircuitBreakerEventMetadata, CircuitBreakerMetadata and CircuitBreakerStats

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,6 @@ export { CBEvent } from './circuit-breaker.constants';
 export { CircuitBreakerModule } from './circuit-breaker.module';
 export { CircuitBreakerService } from './circuit-breaker.service';
 export * from './decorators/circuit-breaker.decorator';
+export { CircuitBreakerEventMetadata, CircuitBreakerMetadata } from './interface/circuit-breaker-metadata.interface';
+export { CircuitBreakerStats } from './interface/circuit-breaker-stats.interface';
 
